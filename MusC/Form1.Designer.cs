463415b1dd9100ad0efa90b00ffb63dadf707610
CDUGUINE <cduguine@gmail.com>
2023-11-18 20:33:27 +0100

    Ajoutez des fichiers projet.

---
+++ MusC/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace MusC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstCartes = new System.Windows.Forms.ListBox();
+            this.btnMelanger = new System.Windows.Forms.Button();
+            this.grpJoueur1 = new System.Windows.Forms.GroupBox();
+            this.btnVoir = new System.Windows.Forms.Button();
+            this.btnMus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstCartes
+            // 
+            this.lstCartes.FormattingEnabled = true;
+            this.lstCartes.ItemHeight = 16;
+            this.lstCartes.Location = new System.Drawing.Point(580, 12);
+            this.lstCartes.Name = "lstCartes";
+            this.lstCartes.Size = new System.Drawing.Size(84, 132);
+            this.lstCartes.TabIndex = 0;
+            // 
+            // btnMelanger
+            // 
+            this.btnMelanger.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnMelanger.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMelanger.Location = new System.Drawing.Point(580, 150);
+            this.btnMelanger.Name = "btnMelanger";
+            this.btnMelanger.Size = new System.Drawing.Size(84, 27);
+            this.btnMelanger.TabIndex = 1;
+            this.btnMelanger.Text = "mélanger";
+            this.btnMelanger.UseVisualStyleBackColor = true;
+            this.btnMelanger.Click += new System.EventHandler(this.btnMelanger_Click);
+            // 
+            // grpJoueur1
+            // 
+            this.grpJoueur1.Location = new System.Drawing.Point(12, 12);
+            this.grpJoueur1.Name = "grpJoueur1";
+            this.grpJoueur1.Size = new System.Drawing.Size(562, 230);
+            this.grpJoueur1.TabIndex = 4;
+            this.grpJoueur1.TabStop = false;
+            this.grpJoueur1.Text = "Joueur 1";
+            // 
+            // btnVoir
+            // 
+            this.btnVoir.Location = new System.Drawing.Point(12, 248);
+            this.btnVoir.Name = "btnVoir";
+            this.btnVoir.Size = new System.Drawing.Size(75, 23);
+            this.btnVoir.TabIndex = 5;
+            this.btnVoir.Text = "voir";
+            this.btnVoir.UseVisualStyleBackColor = true;
+            this.btnVoir.MouseLeave += new System.EventHandler(this.btnVoir_MouseLeave);
+            this.btnVoir.MouseHover += new System.EventHandler(this.btnVoir_MouseHover);
+            // 
+            // btnMus
+            // 
+            this.btnMus.Location = new System.Drawing.Point(93, 248);
+            this.btnMus.Name = "btnMus";
+            this.btnMus.Size = new System.Drawing.Size(75, 23);
+            this.btnMus.TabIndex = 6;
+            this.btnMus.Text = "mus";
+            this.btnMus.UseVisualStyleBackColor = true;
+            this.btnMus.Click += new System.EventHandler(this.btnMus_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 279);
+            this.Controls.Add(this.btnMus);
+            this.Controls.Add(this.btnVoir);
+            this.Controls.Add(this.grpJoueur1);
+            this.Controls.Add(this.btnMelanger);
+            this.Controls.Add(this.lstCartes);
+            this.Name = "Form1";
+            this.Text = "Mus en C#";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstCartes;
+        private System.Windows.Forms.Button btnMelanger;
+        private System.Windows.Forms.GroupBox grpJoueur1;
+        private System.Windows.Forms.Button btnVoir;
+        private System.Windows.Forms.Button btnMus;
+    }
+}
+
---
+++ MusC/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MusC
+{
+    public partial class Form1 : Form
+    {
+        List<int> shuffled = new List<int>() {1};
+        int[] cartes = new int[4];
+        int numCarte = 4, nombreMus = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Evénement click sur le bouton btnMelanger
+        /// appelle la procédure Melanger
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnMelanger_Click(object sender, EventArgs e)
+        {
+            Melanger();
+        }
+
+        public void Melanger()
+        {
+            List<int> values = new List<int>() { 1 };
+            // remplissage de liste values avec les 40 premiers entiers
+            for (int k = 2; k < 41; k++)
+            {
+                values.Add(k);
+            }
+            // affectation dans la liste shuffled de valeurs mélangées de la liste values
+            Random rand = new Random();
+            shuffled = values.OrderBy(_ => rand.Next()).ToList();
+            // mise à jour de la ListBox
+            lstCartes.Items.Clear();
+            for (int k = 0; k < 40; k++)
+            {
+                lstCartes.Items.Add(shuffled[k]);
+            }
+            // affectation des 4 premières valeurs de shuffled dans le tableau cartes
+            for (int k = 0; k < 4; k++)
+            {
+                cartes[k] = shuffled[k];
+            }
+            // rendre les cartes visibles
+            foreach (Button btn in grpJoueur1.Controls)
+            {
+                btn.Visible = true;
+            }
+            btnVoir.Enabled = true;
+        }
+
+        /// <summary>
+        /// Evénement click sur un bouton carte
+        /// déplace la carte vers le bas (échangée) ou vers le haut(conservée)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCarte1_Click(object sender, EventArgs e)
+        {
+            // récupération de l'objet concerné par le clic
+            Button btn = (Button)sender;
+            // modifie la position du bouton et compte les cartes à échanger
+            if(btn.Top == 12)
+            {
+                btn.Top = 80;
+                nombreMus++;
+            }
+            else
+            {
+                btn.Top = 12;
+                nombreMus--;
+            }
+            // active le bouton btnMus s'il y a au moins une carte à échanger
+            btnMus.Enabled = nombreMus > 0;
+        }
+
+        /// <summary>
+        /// Création des 4 cartes dans grpJoueur1
+        /// </summary>
+        private void CreerPlusieursBoutons()
+        {
+            for (int k = 0; k < 4; k++)
+            {
+                // déclaration et création d'un bouton
+                Button btn = new Button();
+                grpJoueur1.Controls.Add(btn);
+                // image
+                btn.BackgroundImage = (Image)Properties.Resources.ResourceManager.GetObject("dos");
+                btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+                // position et autres propriétés
+                btn.Location = new System.Drawing.Point(4+104 * k, 12);
+                btn.Name = "btnCarte" + k;
+                btn.Size = new System.Drawing.Size(102, 168);
+                btn.TabIndex = 3 + k;
+                btn.UseVisualStyleBackColor = true;
+                btn.Visible = false;
+                // affectation de l'événement commun aux boutons cartes
+                btn.Click += new EventHandler(btnCarte1_Click);
+            }
+        }
+        /// <summary>
+        /// affiche les cartes
+        /// </summary>
+        private void MettreAJourImages()
+        {
+            int k = 0;
+            foreach (Button btn in grpJoueur1.Controls)
+            {
+                btn.BackgroundImage = (Image)Properties.Resources.ResourceManager.GetObject(cartes[k].ToString());
+                btn.Visible = true;
+                k++;
+            }
+        }
+        /// <summary>
+        /// cache les cartes en remettant l'image sur dos
+        /// </summary>
+        private void CacherCartes()
+        {
+            foreach (Button btn in grpJoueur1.Controls)
+            {
+                btn.BackgroundImage = (Image)Properties.Resources.ResourceManager.GetObject("dos");
+            }
+        }
+
+        /// <summary>
+        /// chargement du form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreerPlusieursBoutons();
+            btnMus.Enabled = false;
+            btnVoir.Enabled = false;
+        }
+
+        /// <summary>
+        /// Evénement MouseHover sur le bouton btnVoir
+        /// montre les cartes quand on survole ce bouton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnVoir_MouseHover(object sender, EventArgs e)
+        {
+            MettreAJourImages();
+        }
+
+        /// <summary>
+        /// Evénement MouseLeave sur le bouton btnVoir
+        /// cache les cartes quand on quitte ce bouton
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnVoir_MouseLeave(object sender, EventArgs e)
+        {
+            CacherCartes();
+        }
+
+        /// <summary>
+        /// Evénement click sur le bouton btnMus
+        /// Echange les cartes qui sont baissées
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnMus_Click(object sender, EventArgs e)
+        {
+            {
+                int k = 0;
+                foreach (Button btn in grpJoueur1.Controls)
+                {
+                    if(btn.Top == 80)
+                    {
+                        cartes[k] = shuffled[numCarte];
+                        btn.Top = 12;
+                        numCarte++;
+                    }
+                    k++;
+                    MettreAJourImages();
+                    CacherCartes();
+                }
+            }
+        }
+    }
+}
